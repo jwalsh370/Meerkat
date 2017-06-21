6af09dc13dc55c32dd5ea8779eeb1b71c42627f4
Jahan <jwalsh370@gmail.com>
2017-06-21 11:20:59 -0700

    add buttons xml and test uri to main activity

--- app/src/main/java/com/jahanwalsh/meerkat/MainActivity.java
+++ app/src/main/java/com/jahanwalsh/meerkat/MainActivity.java
@@ -1,51 +1,174 @@
 
 package com.jahanwalsh.meerkat;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
-import com.jahanwalsh.meerkat.R;
 import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.RequiresApi;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Transformation;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
 import com.spotify.sdk.android.player.Config;
 import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Connectivity;
 import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Metadata;
+import com.spotify.sdk.android.player.PlaybackBitrate;
+import com.spotify.sdk.android.player.PlaybackState;
 import com.spotify.sdk.android.player.Player;
 import com.spotify.sdk.android.player.PlayerEvent;
 import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.SpotifyPlayer;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends Activity implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
 
     private static final String CLIENT_ID = "939c554123ba49ee9b14bb76bc59012a";
     private static final String REDIRECT_URI = "http://meerkatmusic.com/callback";
+    @SuppressWarnings("SpellCheckingInspection")
+    private static final String TEST_SONG_URI = "spotify:track:6KywfgRqvgvfJc3JRwaZdZ";
+    @SuppressWarnings("SpellCheckingInspection")
+    private static final String TEST_SONG_MONO_URI = "spotify:track:1FqY3uJypma5wkYw66QOUi";
+    @SuppressWarnings("SpellCheckingInspection")
+    private static final String TEST_SONG_48kHz_URI = "spotify:track:3wxTNS3aqb9RbBLZgJdZgH";
+    @SuppressWarnings("SpellCheckingInspection")
+    private static final String TEST_PLAYLIST_URI = "spotify:user:spotify:playlist:2yLXxKhhziG2xzy7eyD4TD";
+    @SuppressWarnings("SpellCheckingInspection")
+    private static final String TEST_ALBUM_URI = "spotify:album:2lYmxilk8cXJlxxXmns1IU";
+    @SuppressWarnings("SpellCheckingInspection")
+    private static final String TEST_QUEUE_SONG_URI = "spotify:track:5EEOjaJyWvfMglmEwf9bG3";
 
+    //CONSTANTS
     private Player mPlayer;
+    private PlaybackState mCurrentPlaybackState;
+    private BroadcastReceiver mNetworkStateReceiver;
     private Button mFindPlaylistButton;
+    private TextView mStatusText;
+
+    private TextView mMetadataText;
+
+    private EditText mSeekEditText;
+    private ScrollView mStatusTextScrollView;
+    private Metadata mMetadata;
     private static final int REQUEST_CODE = 1991;
+
+    private static final int[] REQUIRES_INITIALIZED_STATE = {
+            R.id.play_track_button,
+            R.id.play_mono_track_button,
+            R.id.play_48khz_track_button,
+            R.id.play_album_button,
+            R.id.play_playlist_button,
+            R.id.pause_button,
+            R.id.seek_button,
+            R.id.low_bitrate_button,
+            R.id.normal_bitrate_button,
+            R.id.high_bitrate_button,
+            R.id.seek_edittext,
+    };
+
+    private static final int[] REQUIRES_PLAYING_STATE = {
+            R.id.skip_next_button,
+            R.id.skip_prev_button,
+            R.id.queue_song_button,
+            R.id.toggle_shuffle_button,
+            R.id.toggle_repeat_button,
+    };
+    public static final String TAG = "SpotifySdk";
+
+    private final Player.OperationCallback mOperationCallback = new Player.OperationCallback() {
+        @Override
+        public void onSuccess() {
+            Log.d("OK!", "WORKS");
+        }
+
+        @Override
+        public void onError(Error error) {
+            Log.d("ERROR:", "error");
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFindPlaylistButton = (Button) findViewById(R.id.findPlaylistButton);
-        mFindPlaylistButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello Players!", Toast.LENGTH_LONG).show();
-            }
-        });
 
         AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
                 AuthenticationResponse.Type.TOKEN,
                 REDIRECT_URI);
         builder.setScopes(new String[]{"user-read-private", "streaming"});
         AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+
+        mStatusText = (TextView) findViewById(R.id.status_text);
+        mMetadataText = (TextView) findViewById(R.id.metadata);
+        mSeekEditText = (EditText) findViewById(R.id.seek_edittext);
+        mStatusTextScrollView = (ScrollView) findViewById(R.id.status_text_container);
+
+        updateView();
+        Log.d("Ready", "READY");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Set up the broadcast receiver for network events. Note that we also unregister
+        // this receiver again in onPause().
+        mNetworkStateReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (mPlayer != null) {
+                    Connectivity connectivity = getNetworkConnectivity(getBaseContext());
+                    Log.d("Network state changed: ", connectivity.toString());
+                    mPlayer.setConnectivityStatus(mOperationCallback, connectivity);
+                }
+            }
+        };
+
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(mNetworkStateReceiver, filter);
+
+        if (mPlayer != null) {
+            mPlayer.addNotificationCallback(MainActivity.this);
+            mPlayer.addConnectionStateCallback(MainActivity.this);
+        }
+    }
+
+    private Connectivity getNetworkConnectivity(Context context) {
+        ConnectivityManager connectivityManager;
+        connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+        if (activeNetwork != null && activeNetwork.isConnected()) {
+            return Connectivity.fromNetworkType(activeNetwork.getType());
+        } else {
+            return Connectivity.OFFLINE;
+        }
+    }
+
+    private void openLoginWindow() {
+        final AuthenticationRequest request = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI)
+                .setScopes(new String[]{"user-read-private", "playlist-read", "playlist-read-private", "streaming"})
+                .build();
 
         AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
     }
@@ -76,29 +199,186 @@ public class MainActivity extends Activity implements SpotifyPlayer.Notification
         }
     }
 
-    @Override
-    protected void onDestroy() {
-        Spotify.destroyPlayer(this);
-        super.onDestroy();
-    }
+    private void onAuthenticationComplete(AuthenticationResponse authResponse) {
+        // Once we have obtained an authorization token, we can proceed with creating a Player.
+        Log.d("Got authtoken", "Got");
+        if (mPlayer == null) {
+            Config playerConfig = new Config(getApplicationContext(), authResponse.getAccessToken(), CLIENT_ID);
+            // Since the Player is a static singleton owned by the Spotify class, we pass "this" as
+            // the second argument in order to refcount it properly. Note that the method
+            // Spotify.destroyPlayer() also takes an Object argument, which must be the same as the
+            // one passed in here. If you pass different instances to Spotify.getPlayer() and
+            // Spotify.destroyPlayer(), that will definitely result in resource leaks.
+            mPlayer = Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
+                @Override
+                public void onInitialized(SpotifyPlayer player) {
+                    Log.d("-- Player initialized ", "playerStart");
+                    mPlayer.setConnectivityStatus(mOperationCallback, getNetworkConnectivity(MainActivity.this));
+                    mPlayer.addNotificationCallback(MainActivity.this);
+                    mPlayer.addConnectionStateCallback(MainActivity.this);
+                    // Trigger UI refresh
+                    updateView();
+                }
 
-    @Override
-    public void onPlaybackEvent(PlayerEvent playerEvent) {
-        Log.d("MainActivity", "Playback event received: " + playerEvent.name());
-        switch (playerEvent) {
-            default:
-                break;
+                @Override
+                public void onError(Throwable error) {
+                    Log.d("Error in initialization", error.getMessage());
+                }
+            });
+        } else {
+            mPlayer.login(authResponse.getAccessToken());
         }
     }
 
-    @Override
-    public void onPlaybackError(Error error) {
-        Log.d("MainActivity", "Playback error received: " + error.name());
-        switch (error) {
-            default:
-                break;
+
+    private void updateView() {
+        boolean loggedIn = isLoggedIn();
+
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setText(loggedIn ? R.string.logout_button_label : R.string.login_button_label);
+
+        for (int id : REQUIRES_INITIALIZED_STATE) {
+            findViewById(id).setEnabled(loggedIn);
+        }
+
+        boolean playing = loggedIn && mCurrentPlaybackState != null && mCurrentPlaybackState.isPlaying;
+        for (int id : REQUIRES_PLAYING_STATE) {
+            findViewById(id).setEnabled(playing);
+        }
+
+        if (mMetadata != null) {
+            findViewById(R.id.skip_next_button).setEnabled(mMetadata.nextTrack != null);
+            findViewById(R.id.skip_prev_button).setEnabled(mMetadata.prevTrack != null);
+            findViewById(R.id.pause_button).setEnabled(mMetadata.currentTrack != null);
+        }
+
+        final ImageView coverArtView = (ImageView) findViewById(R.id.cover_art);
+        if (mMetadata != null && mMetadata.currentTrack != null) {
+            final String durationStr = String.format(" (%dms)", mMetadata.currentTrack.durationMs);
+            mMetadataText.setText(mMetadata.contextName + "\n" + mMetadata.currentTrack.name + " - " + mMetadata.currentTrack.artistName + durationStr);
+
+            Picasso.with(this)
+                    .load(mMetadata.currentTrack.albumCoverWebUrl)
+                    .transform(new Transformation() {
+
+                       @Override
+                        public Bitmap transform(Bitmap source) {
+                            final Bitmap copy = source.copy(source.getConfig(), true);
+                            source.recycle();
+                            final Canvas canvas = new Canvas(copy);
+                            canvas.drawColor(0xbb000000);
+                            return copy;
+                        }
+
+                        @Override
+                        public String key() {
+                            return "darken";
+                        }
+                    })
+                    .into(coverArtView);
+        } else {
+            mMetadataText.setText("<nothing is playing>");
+            coverArtView.setBackground(null);
+        }
+
+    }
+
+    private boolean isLoggedIn() {
+
+        return mPlayer != null;
+    }
+
+    public void onLoginButtonClicked(View view) {
+        if (!isLoggedIn()) {
+            Log.d("Logging in", "LOGGED");
+
+            openLoginWindow();
+        } else {
+            mPlayer.logout();
         }
     }
+
+    public void onPlayButtonClicked(View view) {
+
+        String uri;
+        switch (view.getId()) {
+            case R.id.play_track_button:
+                uri = TEST_SONG_URI;
+                break;
+            case R.id.play_mono_track_button:
+                uri = TEST_SONG_MONO_URI;
+                break;
+            case R.id.play_48khz_track_button:
+                uri = TEST_SONG_48kHz_URI;
+                break;
+            case R.id.play_playlist_button:
+                uri = TEST_PLAYLIST_URI;
+                break;
+            case R.id.play_album_button:
+                uri = TEST_ALBUM_URI;
+                break;
+            default:
+                throw new IllegalArgumentException("View ID does not have an associated URI to play");
+        }
+
+        Log.d("Starting playback for ", uri);
+        mPlayer.playUri(mOperationCallback, uri, 0, 0);
+    }
+
+    public void onPauseButtonClicked(View view) {
+        if (mCurrentPlaybackState != null && mCurrentPlaybackState.isPlaying) {
+            mPlayer.pause(mOperationCallback);
+        } else {
+            mPlayer.resume(mOperationCallback);
+        }
+    }
+
+    public void onSkipToPreviousButtonClicked(View view) {
+        mPlayer.skipToPrevious(mOperationCallback);
+    }
+
+    public void onSkipToNextButtonClicked(View view) {
+        mPlayer.skipToNext(mOperationCallback);
+    }
+
+    public void onQueueSongButtonClicked(View view) {
+        mPlayer.queue(mOperationCallback, TEST_QUEUE_SONG_URI);
+        Toast toast = Toast.makeText(this, R.string.song_queued_toast, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void onToggleShuffleButtonClicked(View view) {
+        mPlayer.setShuffle(mOperationCallback, !mCurrentPlaybackState.isShuffling);
+    }
+
+    public void onToggleRepeatButtonClicked(View view) {
+        mPlayer.setRepeat(mOperationCallback, !mCurrentPlaybackState.isRepeating);
+    }
+
+    public void onSeekButtonClicked(View view) {
+        final Integer seek = Integer.valueOf(mSeekEditText.getText().toString());
+        mPlayer.seekToPosition(mOperationCallback, seek);
+    }
+
+    public void onLowBitrateButtonPressed(View view) {
+        mPlayer.setPlaybackBitrate(mOperationCallback, PlaybackBitrate.BITRATE_LOW);
+    }
+
+    public void onNormalBitrateButtonPressed(View view) {
+        mPlayer.setPlaybackBitrate(mOperationCallback, PlaybackBitrate.BITRATE_NORMAL);
+    }
+
+    public void onHighBitrateButtonPressed(View view) {
+        mPlayer.setPlaybackBitrate(mOperationCallback, PlaybackBitrate.BITRATE_HIGH);
+    }
+
+
+//CALLBACK METHODS
+
+
+
+
+
 
     @Override
     public void onLoggedIn() {
@@ -127,5 +407,47 @@ public class MainActivity extends Activity implements SpotifyPlayer.Notification
     @Override
     public void onConnectionMessage(String message) {
         Log.d("MainActivity", "Received connection message: " + message);
+    }
+
+
+    private void logStatus(String status) {
+        Log.i(TAG, status);
+        if (!TextUtils.isEmpty(mStatusText.getText())) {
+            mStatusText.append("\n");
+        }
+        mStatusText.append(">>>" + status);
+        mStatusTextScrollView.post(new Runnable() {
+            @Override
+            public void run() {
+                // Scroll to the bottom
+                mStatusTextScrollView.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+    }
+
+    //destruction
+
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+        mCurrentPlaybackState = mPlayer.getPlaybackState();
+        mMetadata = mPlayer.getMetadata();
+        Log.i(TAG, "Player state: " + mCurrentPlaybackState);
+        Log.i(TAG, "Metadata: " + mMetadata);
+        updateView();
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+        Log.d("MainActivity", "Playback error received: " + error.name());
+        switch (error) {
+            default:
+                break;
+        }
     }
 }
